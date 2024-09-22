65f44c6eeb456cbdc4836d150c440da045199dc4
Aslambek Dombaev <dombaevaslambek@gmail.com>
2024-09-22 22:27:22 +0300

    Task1

---
+++ homework1Task1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class homework1Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+
+        if(a < 0) {
+            System.out.println(-1);
+        }
+        else if (a == 0) {
+            System.out.println(1);
+        }
+        else {
+            int res = 1;
+            for (int i = 2; i <= a; i++) {
+                res *= i;
+            }
+            System.out.println(res);
+        }
+    }
+}
